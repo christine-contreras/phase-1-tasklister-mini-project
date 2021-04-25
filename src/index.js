9dbbf171b0c22f036c9ccc5067993662547c1aa1
Christine Contreras <christineec23@yahoo.com>
2021-04-25 12:31:11 -0500

    js written to execute to li on ul with input text

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
+
+//wait for document to load
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  //As a user, I should be able to type a task into the input field, click some form of a submit button.
+
+  //create variable to hold submit button value
+  const button = document.querySelector('input[type=submit]');
+  //create a variable to hold text input
+  const textInput = document.getElementById('new-task-description');
+
+
+  //make sure I am grabbing the correct elements 
+  console.log(button);
+  console.log(textInput);
+
+  //event listener for the submit button 
+  button.addEventListener('click', (event) => {
+    //prevent default action on submit click
+    event.preventDefault();
+
+    //fire addToTask()
+    addToTask();
+
+
+  }, false)
+
+  //As a user, I expect to see the task string that I provided appear in the DOM after the submit button has been activated.
+  function addToTask() {
+
+    //variable to hold list 
+    const list = document.getElementById('tasks');
+
+    //create li element 
+    const listItem = document.createElement('li');
+    
+    //variable to grab text input value 
+    const textInputValue = textInput.value;
+    console.log("value of input:", textInput);
+
+    //add input value to li 
+    listItem.textContent = textInputValue;
+
+    //append li to ul 
+    list.appendChild(listItem);
+
+
+  }
+
 });
+
